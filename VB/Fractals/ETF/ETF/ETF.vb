fb2902bdf267e4155c00161b4d65ecec367b969c
AppaBabba <darleg@outlook.com>
2018-05-19 10:08:45 -0700

    add end to menu

---
+++ VB/Fractals/ETF/ETF/ETF.vb
@@ -0,0 +1,10 @@
+﻿Public Class ETF
+
+    Private Sub StopToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles StopToolStripMenuItem.Click
+        End
+    End Sub
+
+    Private Sub CleanToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CleanToolStripMenuItem.Click
+
+    End Sub
+End Class

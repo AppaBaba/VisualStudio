ff2f9b7a8aa603b152e4d6ce62be8c859bd3dd23
AppaBabba <darleg@outlook.com>
2018-05-18 20:04:46 -0700

    New ET visual basic
    started updating ET.vb6 to ETF

---
+++ VB/Fractals/ETF/ETF/Form1.Designer.vb
@@ -0,0 +1,222 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FillToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.GrainToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EggToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ButtToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Butt0ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AtractorsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.IkeaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LorenzToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MiraToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Mira0ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Mira1ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Mira2ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.IconToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Icon2ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Icon3ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Icon9ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EndToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem2 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CleanToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.StopToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(24, 24)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FillToolStripMenuItem, Me.ButtToolStripMenuItem, Me.AtractorsToolStripMenuItem, Me.MiraToolStripMenuItem, Me.IconToolStripMenuItem, Me.EndToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(800, 33)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FillToolStripMenuItem
+        '
+        Me.FillToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.GrainToolStripMenuItem, Me.EggToolStripMenuItem})
+        Me.FillToolStripMenuItem.Name = "FillToolStripMenuItem"
+        Me.FillToolStripMenuItem.Size = New System.Drawing.Size(45, 29)
+        Me.FillToolStripMenuItem.Text = "&Fill"
+        '
+        'GrainToolStripMenuItem
+        '
+        Me.GrainToolStripMenuItem.Name = "GrainToolStripMenuItem"
+        Me.GrainToolStripMenuItem.Size = New System.Drawing.Size(252, 30)
+        Me.GrainToolStripMenuItem.Text = "&grain"
+        '
+        'EggToolStripMenuItem
+        '
+        Me.EggToolStripMenuItem.Name = "EggToolStripMenuItem"
+        Me.EggToolStripMenuItem.Size = New System.Drawing.Size(252, 30)
+        Me.EggToolStripMenuItem.Text = "&egg"
+        '
+        'ButtToolStripMenuItem
+        '
+        Me.ButtToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.Butt0ToolStripMenuItem})
+        Me.ButtToolStripMenuItem.Name = "ButtToolStripMenuItem"
+        Me.ButtToolStripMenuItem.Size = New System.Drawing.Size(56, 29)
+        Me.ButtToolStripMenuItem.Text = "&Butt"
+        '
+        'Butt0ToolStripMenuItem
+        '
+        Me.Butt0ToolStripMenuItem.Name = "Butt0ToolStripMenuItem"
+        Me.Butt0ToolStripMenuItem.Size = New System.Drawing.Size(252, 30)
+        Me.Butt0ToolStripMenuItem.Text = "butt&0"
+        '
+        'AtractorsToolStripMenuItem
+        '
+        Me.AtractorsToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.IkeaToolStripMenuItem, Me.LorenzToolStripMenuItem})
+        Me.AtractorsToolStripMenuItem.Name = "AtractorsToolStripMenuItem"
+        Me.AtractorsToolStripMenuItem.Size = New System.Drawing.Size(96, 29)
+        Me.AtractorsToolStripMenuItem.Text = "&Atractors"
+        '
+        'IkeaToolStripMenuItem
+        '
+        Me.IkeaToolStripMenuItem.Name = "IkeaToolStripMenuItem"
+        Me.IkeaToolStripMenuItem.Size = New System.Drawing.Size(252, 30)
+        Me.IkeaToolStripMenuItem.Text = "&ikeda"
+        '
+        'LorenzToolStripMenuItem
+        '
+        Me.LorenzToolStripMenuItem.Name = "LorenzToolStripMenuItem"
+        Me.LorenzToolStripMenuItem.Size = New System.Drawing.Size(252, 30)
+        Me.LorenzToolStripMenuItem.Text = "&lorenz"
+        '
+        'MiraToolStripMenuItem
+        '
+        Me.MiraToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.Mira0ToolStripMenuItem, Me.Mira1ToolStripMenuItem, Me.Mira2ToolStripMenuItem})
+        Me.MiraToolStripMenuItem.Name = "MiraToolStripMenuItem"
+        Me.MiraToolStripMenuItem.Size = New System.Drawing.Size(59, 29)
+        Me.MiraToolStripMenuItem.Text = "&Mira"
+        '
+        'Mira0ToolStripMenuItem
+        '
+        Me.Mira0ToolStripMenuItem.Name = "Mira0ToolStripMenuItem"
+        Me.Mira0ToolStripMenuItem.Size = New System.Drawing.Size(252, 30)
+        Me.Mira0ToolStripMenuItem.Text = "mira&0"
+        '
+        'Mira1ToolStripMenuItem
+        '
+        Me.Mira1ToolStripMenuItem.Name = "Mira1ToolStripMenuItem"
+        Me.Mira1ToolStripMenuItem.Size = New System.Drawing.Size(252, 30)
+        Me.Mira1ToolStripMenuItem.Text = "mira&1"
+        '
+        'Mira2ToolStripMenuItem
+        '
+        Me.Mira2ToolStripMenuItem.Name = "Mira2ToolStripMenuItem"
+        Me.Mira2ToolStripMenuItem.Size = New System.Drawing.Size(252, 30)
+        Me.Mira2ToolStripMenuItem.Text = "mira&2"
+        '
+        'IconToolStripMenuItem
+        '
+        Me.IconToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.Icon2ToolStripMenuItem, Me.Icon3ToolStripMenuItem, Me.Icon9ToolStripMenuItem})
+        Me.IconToolStripMenuItem.Name = "IconToolStripMenuItem"
+        Me.IconToolStripMenuItem.Size = New System.Drawing.Size(58, 29)
+        Me.IconToolStripMenuItem.Text = "&Icon"
+        '
+        'Icon2ToolStripMenuItem
+        '
+        Me.Icon2ToolStripMenuItem.Name = "Icon2ToolStripMenuItem"
+        Me.Icon2ToolStripMenuItem.Size = New System.Drawing.Size(252, 30)
+        Me.Icon2ToolStripMenuItem.Text = "Icon&2"
+        '
+        'Icon3ToolStripMenuItem
+        '
+        Me.Icon3ToolStripMenuItem.Name = "Icon3ToolStripMenuItem"
+        Me.Icon3ToolStripMenuItem.Size = New System.Drawing.Size(252, 30)
+        Me.Icon3ToolStripMenuItem.Text = "Icon&3"
+        '
+        'Icon9ToolStripMenuItem
+        '
+        Me.Icon9ToolStripMenuItem.Name = "Icon9ToolStripMenuItem"
+        Me.Icon9ToolStripMenuItem.Size = New System.Drawing.Size(252, 30)
+        Me.Icon9ToolStripMenuItem.Text = "Icon&9"
+        '
+        'EndToolStripMenuItem
+        '
+        Me.EndToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripMenuItem2, Me.CleanToolStripMenuItem, Me.StopToolStripMenuItem})
+        Me.EndToolStripMenuItem.Name = "EndToolStripMenuItem"
+        Me.EndToolStripMenuItem.Size = New System.Drawing.Size(54, 29)
+        Me.EndToolStripMenuItem.Text = "&End"
+        '
+        'ToolStripMenuItem2
+        '
+        Me.ToolStripMenuItem2.Name = "ToolStripMenuItem2"
+        Me.ToolStripMenuItem2.Size = New System.Drawing.Size(252, 30)
+        Me.ToolStripMenuItem2.Text = "&"
+        '
+        'CleanToolStripMenuItem
+        '
+        Me.CleanToolStripMenuItem.Name = "CleanToolStripMenuItem"
+        Me.CleanToolStripMenuItem.Size = New System.Drawing.Size(252, 30)
+        Me.CleanToolStripMenuItem.Text = "&Clean"
+        '
+        'StopToolStripMenuItem
+        '
+        Me.StopToolStripMenuItem.Name = "StopToolStripMenuItem"
+        Me.StopToolStripMenuItem.Size = New System.Drawing.Size(252, 30)
+        Me.StopToolStripMenuItem.Text = "&Stop"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FillToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents GrainToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EggToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ButtToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Butt0ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AtractorsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents IkeaToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents LorenzToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents MiraToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Mira0ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Mira1ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Mira2ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents IconToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Icon2ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Icon3ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Icon9ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EndToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem2 As ToolStripMenuItem
+    Friend WithEvents CleanToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents StopToolStripMenuItem As ToolStripMenuItem
+End Class

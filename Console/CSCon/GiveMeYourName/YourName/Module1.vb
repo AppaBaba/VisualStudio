3881cd12f11f967422656d6f62b52adf315a159f
AppaBabba <darleg@outlook.com>
2018-05-27 11:10:36 -0700

    GiveMeYourName

---
+++ Console/CSCon/GiveMeYourName/YourName/Module1.vb
@@ -0,0 +1,20 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim YourName As String
+
+        Console.WriteLine("My name is Appa")
+        Console.WriteLine("")
+        Console.WriteLine("What is your name")
+        Console.WriteLine("")
+
+        YourName = Console.ReadLine()
+        Console.Write("Hi ")
+        Console.Write(YourName)
+        Console.Write(" How are you doing today?")
+        Console.ReadLine()
+
+    End Sub
+
+End Module

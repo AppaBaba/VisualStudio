26151b0e25e1eccc90c0f9479e781525ac5e8549
AppaBabba <darleg@outlook.com>
2018-05-27 11:28:00 -0700

    vbconaddtwonumbers

---
+++ Console/VBCon/AddTwoNumbers/Module1.vb
@@ -0,0 +1,21 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim num1, num2, sum As Integer
+
+        Console.WriteLine("Input the first number.")
+        num1 = Console.ReadLine()
+        Console.WriteLine("Input the second number.")
+        num2 = Console.ReadLine()
+
+        sum = num1 + num2
+
+        Console.Write("The sum of the two numbers is ")
+        Console.Write(sum)
+        Console.ReadLine()
+
+
+    End Sub
+
+End Module

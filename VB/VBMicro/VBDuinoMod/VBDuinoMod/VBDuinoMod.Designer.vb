9a02c6287ced00be3a0e48239a474b818ec7db3a
AppaBabba <darleg@outlook.com>
2018-07-13 09:54:57 -0700

    vbduino

---
+++ VB/VBMicro/VBDuinoMod/VBDuinoMod/VBDuinoMod.Designer.vb
@@ -0,0 +1,99 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class VBDuinoMod
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.SP01 = New System.IO.Ports.SerialPort(Me.components)
+        Me.SelectPort = New System.Windows.Forms.ComboBox()
+        Me.ConnectButton = New System.Windows.Forms.Button()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.OnButton = New System.Windows.Forms.Button()
+        Me.OffButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'SelectPort
+        '
+        Me.SelectPort.FormattingEnabled = True
+        Me.SelectPort.Location = New System.Drawing.Point(474, 62)
+        Me.SelectPort.Name = "SelectPort"
+        Me.SelectPort.Size = New System.Drawing.Size(121, 28)
+        Me.SelectPort.TabIndex = 0
+        '
+        'ConnectButton
+        '
+        Me.ConnectButton.Location = New System.Drawing.Point(474, 179)
+        Me.ConnectButton.Name = "ConnectButton"
+        Me.ConnectButton.Size = New System.Drawing.Size(121, 33)
+        Me.ConnectButton.TabIndex = 1
+        Me.ConnectButton.Text = "Connect"
+        Me.ConnectButton.UseVisualStyleBackColor = True
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(44, 115)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(274, 248)
+        Me.RichTextBox1.TabIndex = 4
+        Me.RichTextBox1.Text = ""
+        '
+        'OnButton
+        '
+        Me.OnButton.Location = New System.Drawing.Point(409, 333)
+        Me.OnButton.Name = "OnButton"
+        Me.OnButton.Size = New System.Drawing.Size(75, 30)
+        Me.OnButton.TabIndex = 5
+        Me.OnButton.Text = "ON"
+        Me.OnButton.UseVisualStyleBackColor = True
+        '
+        'OffButton
+        '
+        Me.OffButton.Location = New System.Drawing.Point(549, 333)
+        Me.OffButton.Name = "OffButton"
+        Me.OffButton.Size = New System.Drawing.Size(75, 30)
+        Me.OffButton.TabIndex = 6
+        Me.OffButton.Text = "OFF"
+        Me.OffButton.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.OffButton)
+        Me.Controls.Add(Me.OnButton)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.ConnectButton)
+        Me.Controls.Add(Me.SelectPort)
+        Me.Name = "Form1"
+        Me.Text = "VBDuinoMod"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents SP01 As IO.Ports.SerialPort
+    Friend WithEvents SelectPort As ComboBox
+    Friend WithEvents ConnectButton As Button
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents OnButton As Button
+    Friend WithEvents OffButton As Button
+End Class
---
+++ VB/VBMicro/VBDuinoMod/VBDuinoMod/VBDuinoMod.vb
@@ -0,0 +1,58 @@
+﻿Imports System
+Imports System.IO.Ports
+
+Public Class VBDuinoMod
+
+    Dim myCom As String
+    Dim feadback As String = ""
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        myCom = ""
+        For Each sp As String In My.Computer.Ports.SerialPortNames
+            SelectPort.Items.Add(sp)
+        Next
+    End Sub
+
+
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles SelectPort.SelectedIndexChanged
+        If (SelectPort.SelectedItem <> "") Then
+            myCom = SelectPort.SelectedItem
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles ConnectButton.Click
+
+        SP01.Close()
+        SP01.PortName = myCom
+        SP01.BaudRate = 9600
+        SP01.DataBits = 8
+        SP01.Parity = Parity.None
+        SP01.StopBits = StopBits.One
+        SP01.Handshake = Handshake.None
+        SP01.Encoding = System.Text.Encoding.Default 'very important!
+        SP01.ReadTimeout = 10000
+
+
+
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles OnButton.Click
+        SP01.Open()
+        SP01.Write("1")
+        feadback = SP01.ReadLine()
+        RichTextBox1.AppendText(feadback)
+        SP01.Close()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles OffButton.Click
+        SP01.Open()
+        SP01.Write("0")
+        feadback = SP01.ReadLine()
+        RichTextBox1.AppendText(feadback)
+        SP01.Close()
+    End Sub
+End Class

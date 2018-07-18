d33bf3b0945afc6edbd552a60c56ff6cb836d0c7
AppaBabba <darleg@outlook.com>
2018-07-18 11:19:38 -0700

    New VB Arduino Monitor

---
+++ VB/VBMicro/VBDuinoMon/VBDuinoMon/VBDuinoMon.Designer.vb
@@ -0,0 +1,126 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class VbduinoMon
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.SP01 = New System.IO.Ports.SerialPort(Me.components)
+        Me.SendTB01 = New System.Windows.Forms.TextBox()
+        Me.RTBox01 = New System.Windows.Forms.RichTextBox()
+        Me.ConnectButton = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.SelectPort = New System.Windows.Forms.ComboBox()
+        Me.SendButton = New System.Windows.Forms.Button()
+        Me.SelectBaudRate = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'SP01
+        '
+        '
+        'SendTB01
+        '
+        Me.SendTB01.Location = New System.Drawing.Point(0, 12)
+        Me.SendTB01.Name = "SendTB01"
+        Me.SendTB01.Size = New System.Drawing.Size(700, 26)
+        Me.SendTB01.TabIndex = 0
+        '
+        'RTBox01
+        '
+        Me.RTBox01.Location = New System.Drawing.Point(0, 74)
+        Me.RTBox01.Name = "RTBox01"
+        Me.RTBox01.Size = New System.Drawing.Size(800, 303)
+        Me.RTBox01.TabIndex = 1
+        Me.RTBox01.Text = ""
+        '
+        'ConnectButton
+        '
+        Me.ConnectButton.Location = New System.Drawing.Point(527, 393)
+        Me.ConnectButton.Name = "ConnectButton"
+        Me.ConnectButton.Size = New System.Drawing.Size(101, 39)
+        Me.ConnectButton.TabIndex = 2
+        Me.ConnectButton.Text = "Connect"
+        Me.ConnectButton.UseVisualStyleBackColor = True
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Location = New System.Drawing.Point(653, 393)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(97, 39)
+        Me.ClearButton.TabIndex = 3
+        Me.ClearButton.Text = "Clear"
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'SelectPort
+        '
+        Me.SelectPort.FormattingEnabled = True
+        Me.SelectPort.Location = New System.Drawing.Point(218, 399)
+        Me.SelectPort.Name = "SelectPort"
+        Me.SelectPort.Size = New System.Drawing.Size(121, 28)
+        Me.SelectPort.TabIndex = 4
+        '
+        'SendButton
+        '
+        Me.SendButton.Location = New System.Drawing.Point(713, 15)
+        Me.SendButton.Name = "SendButton"
+        Me.SendButton.Size = New System.Drawing.Size(75, 30)
+        Me.SendButton.TabIndex = 6
+        Me.SendButton.Text = "Send"
+        Me.SendButton.UseVisualStyleBackColor = True
+        '
+        'SelectBaudRate
+        '
+        Me.SelectBaudRate.FormattingEnabled = True
+        Me.SelectBaudRate.Items.AddRange(New Object() {"300", "600", "1200", "2400", "4800", "9600", "19200", "38400"})
+        Me.SelectBaudRate.Location = New System.Drawing.Point(372, 399)
+        Me.SelectBaudRate.Name = "SelectBaudRate"
+        Me.SelectBaudRate.Size = New System.Drawing.Size(121, 28)
+        Me.SelectBaudRate.TabIndex = 7
+        Me.SelectBaudRate.Text = "Select Baud"
+        '
+        'VbduinoMon
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.SelectBaudRate)
+        Me.Controls.Add(Me.SendButton)
+        Me.Controls.Add(Me.SelectPort)
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.ConnectButton)
+        Me.Controls.Add(Me.RTBox01)
+        Me.Controls.Add(Me.SendTB01)
+        Me.Name = "VbduinoMon"
+        Me.Text = "VBDuinoMon"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents SP01 As IO.Ports.SerialPort
+    Friend WithEvents SendTB01 As TextBox
+    Friend WithEvents RTBox01 As RichTextBox
+    Friend WithEvents ConnectButton As Button
+    Friend WithEvents ClearButton As Button
+    Friend WithEvents SelectPort As ComboBox
+    Friend WithEvents SendButton As Button
+    Friend WithEvents SelectBaudRate As ComboBox
+End Class
---
+++ VB/VBMicro/VBDuinoMon/VBDuinoMon/VBDuinoMon.vb
@@ -0,0 +1,75 @@
+﻿Imports System
+Imports System.IO.Ports
+
+Public Class VbduinoMon
+    Dim myCom As String
+
+    Private Sub VbduinoMon_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Windows.Forms.Control.CheckForIllegalCrossThreadCalls = False
+        myCom = ""
+        Try
+            For Each sp As String In My.Computer.Ports.SerialPortNames
+                SelectPort.Items.Add(sp)
+            Next
+            SelectPort.SelectedIndex = 0
+            SelectBaudRate.SelectedItem = "9600"
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles SelectPort.SelectedIndexChanged
+        If (SelectPort.SelectedItem <> "") Then
+            myCom = SelectPort.SelectedItem
+        End If
+    End Sub
+
+    Private Sub ComboBox2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles SelectBaudRate.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles ConnectButton.Click
+        If ConnectButton.Text = "Connect" Then
+            SP01.PortName = SelectPort.SelectedItem
+            SP01.BaudRate = Val(SelectBaudRate.SelectedItem)
+            Try
+
+                SP01.Open()
+                ConnectButton.Text = "Disconnect"
+                SendTB01.Enabled = True
+                SP01.DataBits = 8
+                SP01.Parity = Parity.None
+                SP01.StopBits = StopBits.One
+                SP01.Handshake = Handshake.None
+                SP01.Encoding = System.Text.Encoding.Default 'very important!
+                SP01.ReadTimeout = 10000
+            Catch ex As Exception
+            End Try
+        Else
+            SP01.Close()
+            SendTB01.Enabled = False
+            ConnectButton.Text = "Connect"
+        End If
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+        RTBox01.Text = ""
+    End Sub
+
+    Private Sub SendTB01_KeyDown(sender As Object, e As KeyEventArgs) Handles SendTB01.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            SP01.Write(SendTB01.Text)
+            SendTB01.Clear()
+        End If
+    End Sub
+
+    Private Sub SP01_DataReceived(sender As Object, e As SerialDataReceivedEventArgs) Handles SP01.DataReceived
+        RTBox01.Text &= SP01.ReadExisting()
+    End Sub
+
+    Private Sub SendButton_Click(sender As Object, e As EventArgs) Handles SendButton.Click
+        SP01.Write(SendTB01.Text)
+        SendTB01.Clear()
+    End Sub
+End Class

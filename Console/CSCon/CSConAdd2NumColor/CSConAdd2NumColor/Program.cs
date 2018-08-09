9e11c651d53a94f74c91364015fd65c73a98b18c
AppaBabba <darleg@outlook.com>
2018-08-09 13:03:03 -0700

    Relocated form ConApps

---
+++ Console/CSCon/CSConAdd2NumColor/CSConAdd2NumColor/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSConAdd2NumColor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("+++++++++++++++++++++++++++++++++++++++");
+            Console.Write(" Enter the first number to be added:  ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write(" Enter the second number to be added: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("+++++++++++++++++++++++++++++++++++++++");
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            c = a + b;
+            Console.WriteLine("+++++++++++++++++++++++++++++++++++++++");
+            Console.WriteLine("      The sum of two numbers is:     " + c);
+            Console.WriteLine("+++++++++++++++++++++++++++++++++++++++");
+            Console.ReadLine();
+
+        }
+    }
+}

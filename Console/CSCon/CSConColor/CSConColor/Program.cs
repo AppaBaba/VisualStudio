86e2dfaa57aa3f9f86fbbd91de91891ae3bc0279
AppaBabba <darleg@outlook.com>
2018-08-09 14:30:24 -0700

    Relocated from ConApps

---
+++ Console/CSCon/CSConColor/CSConColor/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSConColor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("************************");
+            Console.WriteLine("*Black text over Green.*");
+            Console.WriteLine("************************");
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("************************");
+            Console.WriteLine("*Yellow text over Blue.*");
+            Console.WriteLine("************************");
+
+            Console.ReadLine();
+        }
+    }
+}

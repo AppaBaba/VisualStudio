1f81dc98d63e05fb2ef53a04e1d24d13d009cfff
AppaBaba <darleg@outlook.com>
2019-05-13 10:04:03 -0700

    add input

--- Python3/Polar/PyCardioids/PyCardioids/PyCardioids.py
+++ Python3/Polar/PyCardioids/PyCardioids/PyCardioids.py
@@ -1,7 +1,11 @@
 from numpy import arange, sin, pi
 from pylab import polar, show
-a = 6
-b = 1
+
+astring = input ('Enter a: ')
+bstring = input ('Enter b: ')
+a = int(astring)
+b = int(bstring)
+
 t = arange(0, 2*pi, .01)
 polar(t, a + (b * sin(t)))
 show()

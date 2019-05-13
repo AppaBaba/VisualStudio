9ac719c5451cbf37e8ba7159e478e305d838bd8a
AppaBaba <darleg@outlook.com>
2019-05-13 09:41:35 -0700

    add input

--- Python3/Polar/PyPetals/PyPetals/PyPetals.py
+++ Python3/Polar/PyPetals/PyPetals/PyPetals.py
@@ -1,7 +1,11 @@
 from numpy import arange, sin, pi
 from pylab import polar, show
-a = 1
-n = 3
+
+astring = input ('Enter a: ')
+nstring = input ('Enter n: ')
+a = int(astring)
+n = int(nstring)
+
 t = arange(0, 2*pi, .01)
 polar(t, a * sin(t * n))
 show()

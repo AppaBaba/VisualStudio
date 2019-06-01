cdbf5360242e9fbd59df286a3089e2a335e3c8c7
AppaBaba <darleg@outlook.com>
2019-06-01 11:13:55 -0700

    add new turtle

---
+++ Python3/IDLE/Turtle/PySpiralShapes.py
@@ -0,0 +1,17 @@
+from turtle import *
+bgcolor("Black")
+color("Gold")
+speed(10)
+s = [3, 4, 6]
+a = 120
+for k in s:
+    d = 0
+    setpos(0,0)
+    clear()
+    for i in range(20):
+        for j in range(k):
+            forward(d)
+            right(a)
+            d = d + 8 - k
+    a = a - 30
+    
---
+++ Python3/IDLE/Turtle/PySqSiral.py
@@ -0,0 +1,12 @@
+from turtle import *
+bgcolor("Black")
+color("Gold")
+speed(10)
+s = 0
+for i in range(50):
+    for j in range(4):
+        forward(s)
+        right(90)
+        s = s + 2
+        right(5)
+forward(s/2)
